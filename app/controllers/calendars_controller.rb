e04e77c71e72f511d71c41cf221db80fad503ba5
yushi777 <ucy77777@gmail.com>
2022-12-28 10:09:15 +0900

    commit

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -42,6 +42,7 @@ class CalendarsController < ApplicationController
 
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, wday: wdays[(@todays_date + x).wday], plans: today_plans }
       @week_days.push(days)
+      
     end
 
   end
